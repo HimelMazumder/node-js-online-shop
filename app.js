016925fa5ba5b6db92587d255050c8f116ef3169
Himel Mazumder <57599389+HimelMazumder@users.noreply.github.com>
2024-12-12 00:00:11 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongodbSessionStore = require("connect-mongodb-session")(session);
+
+const errorController = require('./controllers/error');
+const User = require("./models/user");
+
+const app = express();
+const MONGODB_URI = "mongodb+srv://himelMazumder:v43Sp6Y4qJWswrFT@cluster0.tduuh.mongodb.net/node_test_mon?retryWrites=true&w=majority&appName=Cluster0";
+const mongodbSessionStore = new MongodbSessionStore({
+  uri: "mongodb+srv://himelMazumder:v43Sp6Y4qJWswrFT@cluster0.tduuh.mongodb.net/node_test_mon",
+  collection: "sessions",
+})
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: "'20pZ3?s}YZi_}iddUQ,C05P1",
+  resave: false,
+  saveUninitialized: false,
+  store: mongodbSessionStore,
+  cookie: {
+    httpOnly: true,
+  }
+}));
+
+app.use((req, res, next) => {
+  if (req.session.user) {
+    return User.findById(req.session.user._id)
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => console.log(err));
+  }
+  next();
+});
+
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose.connect(MONGODB_URI)
+  .then(result => {
+    return User.findById("6759bf7fbfc6cfc784c62ebf");
+  })
+  .then(user => {
+    if (!user) {
+      const user = new User({
+        name: "Kratu",
+        email: "kratu@krishnadas.com",
+        cart: {
+          items: []
+        }
+      });
+
+      user.save()
+    }
+
+    console.log("DB Connected");
+    app.listen(3000);
+  })
+  .catch(err => console.log(err));
---
+++ controllers/shop.js
@@ -0,0 +1,156 @@
+const Product = require('../models/product');
+const User = require('../models/user');
+const Order = require("../models/order");
+
+exports.getProducts = (req, res, next) => {
+  Product.find()
+    .then(products => {
+      const isAuthenticated = req.session.isLoggedIn;
+
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products',
+        isAuthenticated
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.getIndex = (req, res, next) => {
+
+  Product.find()
+    .then(products => {
+      const isAuthenticated = req.session.isLoggedIn;
+
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        isAuthenticated
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then(product => {
+      const isAuthenticated = req.session.isLoggedIn;
+      console.log(isAuthenticated);
+
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products',
+        isAuthenticated
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId)
+    .then(product => {
+      if (!product) {
+        throw new Error("product not found in DB!");
+      }
+      console.log(req.user);
+      return req.user.addToCart(product);
+    })
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+
+};
+
+exports.getCart = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId", "title")
+    .then(userReturned => {
+      userReturned.cart.items = userReturned.cart.items.filter(item => item.productId !== null);
+      userReturned.save();
+
+      const isAuthenticated = req.session.isLoggedIn;
+
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: userReturned.cart.items,
+        isAuthenticated
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user.deleteItemFromCart(prodId)
+    .then(result => {
+      res.redirect("/cart");
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then(userReturned => {
+      let total = 0;
+      const products = userReturned.cart.items.map(item => {
+        total += item.productId.price * item.quantity;
+        return {
+          productData: { ...item.productId._doc },
+          quantity: item.quantity
+        }
+      });
+
+      const user = {
+        userId: userReturned,
+        name: userReturned.name
+      };
+
+      const newOrder = new Order({
+        products,
+        user,
+        total
+      });
+
+      return newOrder.save();
+    })
+    .then(result => {
+      return req.user.clearCart();
+    })
+    .then(result => {
+      res.redirect('/orders');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.getOrders = (req, res, next) => {
+  const filter = {
+    "user.userId": req.user._id
+  }
+  Order.find(filter)
+    .select("products total")
+    .then(orders => {
+      const isAuthenticated = req.session.isLoggedIn;
+
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders,
+        isAuthenticated
+      });
+    })
+    .catch(err => console.log(err));
+};
