9acd3406f08053dabe22b2651d3236374cf70323
Himel Mazumder <57599389+HimelMazumder@users.noreply.github.com>
2024-12-13 20:27:07 +0600

    Delete controllers directory

--- controllers/error.js
+++
@@ -1,6 +0,0 @@
-exports.get404 = (req, res, next) => {
-  const isAuthenticated = req.session.isLoggedIn;
-  console.log(isAuthenticated);
-  
-  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404', isAuthenticated });
-};
